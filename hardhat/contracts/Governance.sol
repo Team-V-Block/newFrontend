b4345e226b5903c35f790e8b4d0940b91eac9de5
nwokoyepraise <nwokoyepraise@gmail.com>
2022-04-20 13:35:21 +0100

    grant chairman role to contract deployer

--- hardhat/contracts/Governance.sol
+++ hardhat/contracts/Governance.sol
@@ -1,36 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Governance {
-    //Hash roles using keccak256 hashing
+//Hash roles using keccak256 hashing
     bytes32 public constant STUDENT_ROLE = keccak256("STUDENT_ROLE");
     bytes32 public constant TEACHER_ROLE = keccak256("TEACHER_ROLE");
     bytes32 public constant CHAIRMAN_ROLE = keccak256("CHAIRMAN_ROLE");
 
     address[] accounts;
     mapping(bytes32 => address[]) private roles;
-
-    constructor() {
-        //Grant admin role to the contract deployer
-        // _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        // _grantRole(CHAIRMAN_ROLE, msg.sender);
+    constructor (){
+        //Grant chairman role to the contract deployer
+       grantRole(CHAIRMAN_ROLE, msg.sender);
     }
 
     function grantRole(bytes32 role, address account) public returns (bool) {
+        if (role == CHAIRMAN_ROLE) {
+            require (roles[role].length == 0, "Can't assign CHAIRMAN_ROLE to more than one account");
+        }
         roles[role].push(account);
         return true;
     }
-
-    function getRoleMembers(bytes32 role)
-        public
-        view
-        returns (address[] memory)
-    {
+    function getRoleMembers (bytes32 role) public view returns (address [] memory) {
         return roles[role];
     }
 
-    function getRoleChairman() public view returns (address) {
-        // address chairman = _roles(CHAIRMAN_ROLE).members[1];
-        //  return _roles[CHAIRMAN_ROLE].members[msg.sender];
+    function getRoleChairman () public view returns(address){
+     
     }
+
 }
