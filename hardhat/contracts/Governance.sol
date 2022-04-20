6a72643e66799f0f4a4cbc1bf910b46e1ba76b5d
nwokoyepraise <nwokoyepraise@gmail.com>
2022-04-20 17:58:25 +0100

    restructure data stores

--- hardhat/contracts/Governance.sol
+++ hardhat/contracts/Governance.sol
@@ -1,33 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-
 contract Governance {
-//Hash roles using keccak256 hashing
+    //Hash roles using keccak256 hashing
     bytes32 public constant STUDENT_ROLE = keccak256("STUDENT_ROLE");
     bytes32 public constant TEACHER_ROLE = keccak256("TEACHER_ROLE");
     bytes32 public constant CHAIRMAN_ROLE = keccak256("CHAIRMAN_ROLE");
+    
+    struct candidate {
+        bytes32 name;   // short name (up to 32 bytes)
+        uint256 voteCount; // number of accumulated votes
+    }
 
-    address[] accounts;
-    mapping(bytes32 => address[]) private roles;
+    struct voter {
+        bool voted;  // if true, that person already voted
+        uint256 vote;   // index of the voted proposal
+    }
+
+    // A dynamically-sized array of `candidate` structs.
+    candidate[] public candidates;
+    address[] shareholderArray;
+
+    mapping(address => bytes32) shareholders;
+    mapping(address => voter) public voters;
+
+    modifier isValidRole(bytes32 role) {
+        require (role == STUDENT_ROLE || role == TEACHER_ROLE || role == CHAIRMAN_ROLE, "Cannot assign invalid role!");
+        _;
+    }
+
     constructor (){
         //Grant chairman role to the contract deployer
        grantRole(CHAIRMAN_ROLE, msg.sender);
     }
 
-    function grantRole(bytes32 role, address account) public returns (bool) {
-        if (role == CHAIRMAN_ROLE) {
-            require (roles[role].length == 0, "Can't assign CHAIRMAN_ROLE to more than one account");
-        }
-        roles[role].push(account);
+    function grantRole(bytes32 role, address account) public isValidRole(role) returns (bool) {
+        shareholders[account] = role;
+        shareholderArray.push(account);
+        voters[account] = voter(false, 0);
         return true;
     }
-    function getRoleMembers (bytes32 role) public view returns (address [] memory) {
-        return roles[role];
-    }
 
-    function getRoleChairman () public view returns(address){
-     
+    function getRole (address account) public view returns (bytes32) {
+        return shareholders[account];
     }
-
 }
