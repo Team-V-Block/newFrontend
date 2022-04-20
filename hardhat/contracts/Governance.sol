69dcf8b374cfd0bc7c73008dc2642e333b2c4afd
nwokoyepraise <nwokoyepraise@gmail.com>
2022-04-20 23:34:09 +0100

    setup function to vote

--- hardhat/contracts/Governance.sol
+++ hardhat/contracts/Governance.sol
@@ -8,13 +8,13 @@ contract Governance {
     bytes32 public constant CHAIRMAN_ROLE = keccak256("CHAIRMAN_ROLE");
     
     struct candidate {
-        bytes32 name;   // short name (up to 32 bytes)
+        string name;   // short name (up to 32 bytes)
         uint256 voteCount; // number of accumulated votes
     }
 
     struct voter {
         bool voted;  // if true, that person already voted
-        uint256 vote;   // index of the voted proposal
+        uint256 vote;   // index of the voted candidate
     }
 
     // A dynamically-sized array of `candidate` structs.
@@ -23,9 +23,19 @@ contract Governance {
 
     mapping(address => bytes32) shareholders;
     mapping(address => voter) public voters;
-
+   
     modifier isValidRole(bytes32 role) {
         require (role == STUDENT_ROLE || role == TEACHER_ROLE || role == CHAIRMAN_ROLE, "Cannot assign invalid role!");
+        _;
+    }
+
+    modifier isShareholder () {
+        require (shareholders[msg.sender] != 0, "Voter must be a shareholder");
+        _;
+    }
+
+    modifier hasNotVoted () {
+        require (!voters[msg.sender].voted, "Shareholder has already voted");
         _;
     }
 
@@ -35,9 +45,15 @@ contract Governance {
     }
 
     function grantRole(bytes32 role, address account) public isValidRole(role) returns (bool) {
+        require (account != address(0));
         shareholders[account] = role;
         shareholderArray.push(account);
-        voters[account] = voter(false, 0);
+        return true;
+    }
+
+    function vote(uint256 candidateIndex) public isShareholder hasNotVoted returns (bool) {
+        candidates[candidateIndex].voteCount += 1;
+        voters[msg.sender] = voter({voted: true, vote: candidateIndex});
         return true;
     }
 
