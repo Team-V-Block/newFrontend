06152d0ece86bb13813de787c2fe77923fe81243
motolaji <jajimalik153@gmail.com>
2022-04-22 14:32:53 +0100

    update

--- frontend/src/components/governance.js
+++ frontend/src/components/governance.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ABI from '../ABI/Governance.json'
 import { ethers } from "ethers";
+import { BigNumber } from "ethers";
 
 
 
@@ -13,6 +14,8 @@ class Vote extends Component {
     };
   }
 
+
+
   async connectToMetamask() {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
@@ -21,19 +24,33 @@ class Vote extends Component {
     const addressBalance = ethers.utils.formatEther(balance);
     
 
-    
-  
-     const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, provider);
+    ////////************************READ FUNCTION */
+    const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, provider);
     const Chairman = await voteContract.CHAIRMAN_ROLE();
     const Student = await voteContract.STUDENT_ROLE();
     const Teacher = await voteContract.TEACHER_ROLE();
+    
+
+  
     const returnChairman = (Chairman)
     const returnStudent = (Student)
     const returnTeacher = (Teacher)
-    this.setState({ selectedAddress: accounts[0], balance: addressBalance, Chairman:returnChairman, Student:returnStudent, Teacher:returnTeacher  });
+    this.setState({ selectedAddress: accounts[0], balance: addressBalance, Chairman:returnChairman, Student:returnStudent, Teacher:returnTeacher,});
 
 
 }
+async Result() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, provider);
+    const Result = await  voteContract.votingResult();
+
+    this.setState({Result})
+}
+
+
+
+//*********************************WRITE FUNCTIONS */
+
 // grantRole
 async grant() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -44,28 +61,53 @@ async grant() {
     
     this.setState({byte});
 }
-// addCandidates 
-async addCandidates() {
+//changeResultStatus
+async changeResultof() {
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner()
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner()
 
-    const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, signer);
-    const add = await voteContract.addCandidates(["Hollio"])
+const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, signer);
+const change = await voteContract.changeResultStatus(true)
 
-    this.setState({add});
+this.setState({change});
 }
+
 // vote
 async voteCandidate() {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner()
     
     const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, signer);
-    const vote = await voteContract.vote(['3']);
-    this.setState({vote});
-    console.log(vote)
+    const votecand = await voteContract.vote(BigNumber.from("2"));
+    this.setState({votecand});
+    console.log(votecand);
 }
-//
+// addCandidates 
+async addCandidates() {
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner()
+
+const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, signer);
+const add = await voteContract.addCandidates(["Hollio"])
+
+this.setState({add});
+}
+
+//changeVotingAllowed
+
+async changeVoting() {
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner()
+
+const voteContract = new ethers.Contract('0x64bC644e2225D7e6B75A8543221556e0E1A5a955', ABI.abi, signer);
+const voting = await voteContract.changeVotingAllowed(false)
+
+this.setState({voting});
+}
+
 
 
 
@@ -90,9 +132,19 @@ async voteCandidate() {
         <p>Chairman: {this.state.Chairman}</p>
         <p>Student: {this.state.Student}</p>
         <p>Teacher: {this.state.Teacher}</p>
+        <div>
+        {/* Result:
+        {this.state.Result.map ((item) =>(
+            <div key={item.Bignumber}>
+            <p>{item.Bignumber}</p>
+            </div>
+        ))} */}
+        </div>
         <button onClick={() => this.grant()}>grantRole</button>
         <button onClick={() => this.addCandidates()}>addCandidates</button>
-        <button onClick={() => this.voteCandidate}>Vote Candidates</button>
+        <button onClick={() => this.voteCandidate()}>Vote Candidates</button>
+        <button onClick={() => this.changeResultof()}>changeResultStatus</button>
+        <button onClick={() => this.changeVoting()}>ChangeVotingAllowed</button>
         </div>
       );
     }
